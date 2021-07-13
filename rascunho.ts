e5410aa3272c84c92ea42d7ad5bbe8896a89fec1
ighorsantiago <ighor_santiago@hotmail.com>
2021-07-13 12:43:30 -0300

    gofinances

---
+++ rascunho.ts
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// import {Container} from './styles';
+
+// export function CategorySelect() {
+
+//   return (
+//     <Container></Container>
+//   );
+// }
+
+// import styled from 'styled-components/native';
+
+// export const Container = styled.View``;
